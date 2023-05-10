987bc1ba3bdedf1f066a63decfa208eac5c35b56
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-05-09 18:48:56 -0700

    Revert "Updated ProjectScannerSaveToSqlServer to NET 6."
    This reverts commit a9718dcd143018ce0b86695639a8a46f5c24b2ff.

--- IT.TFM/ProjectScannerSaveToSqlServer/Save.cs
+++ IT.TFM/ProjectScannerSaveToSqlServer/Save.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Runtime.Remoting.Messaging;
 using System.Data.Entity.Infrastructure;
 
 namespace ProjectScannerSaveToSqlServer
