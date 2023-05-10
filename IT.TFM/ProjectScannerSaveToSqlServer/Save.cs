734aa3b0bef4cd0cf212da0108d3ee550447963b
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-05-09 19:23:52 -0700

    Updated 6 Projects to NET 6: Parser, ProjectScannerSaveToSqlServer, NpmFileParser, NuGetFileParser, ConfigurationFileData, ProjectScanner

--- IT.TFM/ProjectScannerSaveToSqlServer/Save.cs
+++ IT.TFM/ProjectScannerSaveToSqlServer/Save.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
-using System.Runtime.Remoting.Messaging;
 using System.Data.Entity.Infrastructure;
 
 namespace ProjectScannerSaveToSqlServer
