d217989ad623dd99e2d0a41a406959b3ee234c80
Andreas Mertens <Andreas.Mertens@worksafebc.com>
2023-09-20 11:59:40 -0700

    Updated field width

--- IT.TFM/ProjectScannerDB/dbo/Tables/dotNetEndOfLife.sql
+++ IT.TFM/ProjectScannerDB/dbo/Tables/dotNetEndOfLife.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[dotNetEndOfLife]
 (
-	[Version] NVARCHAR(20) NOT NULL PRIMARY KEY, 
+	[Version] NVARCHAR(50) NOT NULL PRIMARY KEY, 
     [EOL] DATE NOT NULL, 
     [ExtendedEOL] DATE NULL, 
     [Display] NVARCHAR(50) NOT NULL
