2b9ba65c3cb2bc76efc5b189ef2b329280c10cf4
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2023-12-16 10:34:01 -0800

    Adding debug information to diagnose some configuration loading issues

--- IT.TFM/ConfigurationFileData/ConfigurationSettings.cs
+++ IT.TFM/ConfigurationFileData/ConfigurationSettings.cs
@@ -4,13 +4,20 @@ using System.Configuration;
 using System.IO;
 using System.Xml;
 
+using static System.Collections.Specialized.BitVector32;
+
 namespace ConfigurationFileData
 {
     public static class ConfigurationSettings
     {
         public static string[] LoadConfigurationValues(string file, string section)
         {
+            System.Diagnostics.Debug.WriteLine($"=> LoadConfigurationValues: file={file}, section={section}");
+            
             var configuration = GetConfiguration(file);
+
+            System.Diagnostics.Debug.WriteLine($"=> LoadConfigurationValues: configuration loaded = {configuration != null}");
+
             return GetSectionValues(section, configuration);
         }
 
@@ -40,6 +47,8 @@ namespace ConfigurationFileData
         {
             var configData = GetSectionData(sectionName, configuration);
 
+            System.Diagnostics.Debug.WriteLine($"=> GetSectionValues: configData loaded = {configData != null}");
+
             var values = new List<string>();
             foreach (var key in configData.AllKeys)
             {
@@ -67,12 +76,22 @@ namespace ConfigurationFileData
         {
             var section = configuration.GetSection(sectionName);
 
+            System.Diagnostics.Debug.WriteLine($"=> GetSectionData: section loaded = {section != null}");
+
             string sectionRawXml = section.SectionInformation.GetRawXml();
+
+            System.Diagnostics.Debug.WriteLine($"=> GetSectionData: sectionRawXml = {sectionRawXml}");
+
             var sectionXmlDoc = new XmlDocument();
             sectionXmlDoc.Load(new StringReader(sectionRawXml));
 
+            System.Diagnostics.Debug.WriteLine($"=> GetSectionData: sectionXmlDoc loaded");
+
             var handler = new NameValueSectionHandler();
             var configParserSection = handler.Create(null, null, sectionXmlDoc.DocumentElement) as NameValueCollection;
+
+            System.Diagnostics.Debug.WriteLine($"=> GetSectionData: configParserSection loaded = {configParserSection != null}");
+
             return configParserSection;
         }
     }
