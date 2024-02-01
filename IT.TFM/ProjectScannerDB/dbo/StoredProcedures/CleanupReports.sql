ab1828fa963544313c92413a8bd312ac1a06ed25
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-02-01 10:18:23 -0800

    Adding stored proc to clean up old historical report data. Takes a parameter (INT) indicating how many days to maintain in the report. Ex: 30 would mean keep the last 30 days of data.

---
+++ IT.TFM/ProjectScannerDB/dbo/StoredProcedures/CleanupReports.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[CleanupReports]
+	@days int
+AS
+	DECLARE @oldDate DATE = DATEADD(DAY, -@days, GETDATE())
+
+DELETE FROM [dbo].[Reports]
+	WHERE [Timestamp] < @oldDate
+
+RETURN 0
