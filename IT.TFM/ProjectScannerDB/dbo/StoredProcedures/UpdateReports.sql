6d99370c4aa5a41d2aceb018278db92a00ae3be0
Andreas Mertens <115173538+AndreasNVI@users.noreply.github.com>
2024-02-01 15:53:57 -0800

    Historical report data (#24)
    * Some updates to the IsSupportedValue value. This allows the value to be calculated instead of manually updated.
    - Removed column from dotNetEndOfLife table
    - Added the column as one calculated by the diff between the EOL column and the current date in the FullScan view.
    
    Also in FullScan view, update the columns for IsSupportedVersion and IsTargetVersion to be user friendly.
    
    Updated the SQL to populate the dotNetEndOfLife table with updated values and removing the IsSupportedVersion value.
    
    * #664002
    Creating new Reports table, to provide the timestamped report data.
    
    * Minor tweak to the Reports table, removing OrganizationId
    Created UpdateReports SPROC to add the data to the Reports table, with timestamp.
    
    Will now test with a deployment.
    
    * Some tweaks to table and SPROC to fix a few issues and get it running.
    
    * Adding stored proc to clean up old historical report data. Takes a parameter (INT) indicating how many days to maintain in the report. Ex: 30 would mean keep the last 30 days of data.
    
    * Adding a Utility app - ReportsRefresh
    Has two modes:
    1. pass in parameter u, will update the Reports table with the new data.
    2. pass in parameter c X, will clean old data out of the Reports table, where X is the number of days to maintain.
    
    This will used in the Execute TFM pipeline, run after the main scan completes.
    
    ---------
    
    Co-authored-by: Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>

---
+++ IT.TFM/ProjectScannerDB/dbo/StoredProcedures/UpdateReports.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[UpdateReports]
+AS
+	DECLARE @timestamp DATE = GETDATE()
+
+
+	INSERT INTO dbo.Reports (Timestamp, Organization, Project, ProjectId, ProjectDescription, ProjectUrl,
+							 Repository, RepositoryId, RepositoryDefaultBranch, RepositoryUrl,
+	                         FileType, FileId, FilePath, FileUrl,
+							 AzureFunction, IsAzureFunction, FrameworkVersion, IsEol, EolDate, Eol3Months, Eol6Months, Eol9Months, Eol12Months,
+							 SupportedFramework, TargetFramework, FileReference, UrlReference, PackageType, PackageName, PackageVersion)							 
+
+	SELECT		@timestamp,
+				[Org. Name],
+				[Project Name],
+				[Project ID],
+				[Project Description],
+				[Project Url],
+				[Repository Name],
+				[Repository ID],
+				[Repository Default Branch],
+				[Repository URL],
+				[File Type],
+				[File ID],
+				[File Path],
+				[File URL],
+				[PROP_AzureFunction],
+				CASE WHEN [IsAzureFunction] = 'Y' THEN 1 ELSE 0 END,
+				[Package Version],
+				CASE WHEN [Package - EOL] = 'YES' THEN 1 ELSE 0 END,
+				[Package - Date EOL],
+				CASE WHEN [Package EOL - 3 Months] = 'YES' THEN 1 ELSE 0 END,
+				CASE WHEN [Package EOL - 6 Months] = 'YES' THEN 1 ELSE 0 END,
+				CASE WHEN [Package EOL - 9 Months] = 'YES' THEN 1 ELSE 0 END,
+				CASE WHEN [Package EOL - 12 Months] = 'YES' THEN 1 ELSE 0 END,
+				[Is Supported Version],
+				[Is Target Version],
+				[File Reference],
+				[URL Reference],
+				[Pkg Type],
+				[Pkg Reference],
+				[Pkg Version]
+
+	FROM		[dbo].[FullScan]
+RETURN 0
