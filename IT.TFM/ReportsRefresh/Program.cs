a7c68935d7f76789b6c20fcaa06266fc2b0ee345
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-02-01 11:26:50 -0800

    Adding a Utility app - ReportsRefresh Has two modes: 1. pass in parameter u, will update the Reports table with the new data. 2. pass in parameter c X, will clean old data out of the Reports table, where X is the number of days to maintain.
    This will used in the Execute TFM pipeline, run after the main scan completes.

---
+++ IT.TFM/ReportsRefresh/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+if (args.Length == 0)
+{
+    throw new ArgumentException("Need to specify u for update, or c for cleanup as a parameter");
+}
+
+var connectionString = ConfidentialSettings.Values.DbConnection;
+
+switch (args[0])
+{
+    case "u":
+        Update(connectionString);
+        return;
+
+    case "c":
+        if (args.Length < 2 || !int.TryParse(args[1], out var value))
+        {
+            throw new ArgumentException("For cleanup, you need to specify a second int parameter indicating number of days to maintain. Ex: c 30");
+        }
+        Cleanup(connectionString, value);
+        return;
+
+    default: throw new ArgumentException("Invalid argument given, must be either u for update, or c for cleanup");
+}
+
+void Update(string connectionString)
+{
+    using SqlConnection connection = new(connectionString);
+    var command = new SqlCommand("dbo.UpdateReports", connection)
+    {
+        CommandType = CommandType.StoredProcedure,
+        CommandTimeout = 60
+    };
+
+    command.Connection.Open();
+    command.ExecuteNonQuery();
+}
+
+void Cleanup(string connectionString, int days)
+{
+    using SqlConnection connection = new(connectionString);
+    var command = new SqlCommand("dbo.CleanupReports", connection)
+    {
+        CommandType = CommandType.StoredProcedure,
+        CommandTimeout = 60
+    };
+    var daysParameter = command.Parameters.Add("@days", SqlDbType.Int);
+    daysParameter.Value = days;
+
+    command.Connection.Open();
+    command.ExecuteNonQuery();
+}
