b63f7b6d424d040cde04311c0ffd432c0a0e3899
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-05-02 10:25:29 -0700

    Updating the ReportScan view, to use the data in the Reports table. This will be used by the EOL Agent.

--- IT.TFM/ProjectScannerDB/dbo/Views/ReportScan.sql
+++ IT.TFM/ProjectScannerDB/dbo/Views/ReportScan.sql
@@ -1,44 +1,54 @@
 ﻿CREATE VIEW [dbo].[ReportScan]
 	AS
-SELECT		O.[Name] AS [Organization],
-			P.[ProjectId] AS [ProjectId],
-			P.[Name] AS [ProjectName],
-			R.[RepositoryId] AS [RepositoryId],
-			R.[Name] AS RepositoryName,
-			FT.[Id] AS [FileTypeId],
-			F.[Id] AS [FileId],
-			F.[Path] AS [FilePath],
-			FPP.AzureFunction AS AzureFunctionRuntime,
-			CASE WHEN FPP.AzureFunction LIKE 'v%' THEN 1 ELSE 0 END AS IsAzureFunction,
-			EOL.[Display] AS [FrameworkVersion],
-			CASE WHEN EOL.EOL IS NOT NULL AND EOL.EOL <= GETDATE() THEN 1 ELSE 0 END AS [IsEol],
-			EOL.EOL AS [EolDate],
-			CASE WHEN EOL.EOL <= DATEADD(MONTH, 3, GETDATE()) THEN 1 ELSE 0 END AS [Eol3Months],
-			CASE WHEN EOL.EOL <= DATEADD(MONTH, 6, GETDATE()) THEN 1 ELSE 0 END AS [Eol6Months],
-			CASE WHEN EOL.EOL <= DATEADD(MONTH, 9, GETDATE()) THEN 1 ELSE 0 END AS [Eol9Months],
-			CASE WHEN EOL.EOL <= DATEADD(MONTH, 12, GETDATE()) THEN 1 ELSE 0 END AS [Eol12Months],
-			CASE WHEN EOL IS NULL THEN 1
-                 WHEN DATEDIFF(day, EOL, GETDATE()) > 0 THEN 0 ELSE 1 END AS [SupportedFramework],
-			EOL.IsTargetVersion AS [TargetFramework],
-			FR.[Name] AS [FileReference],
-			FU.[Name] AS [UrlReference],
-			FN.[PackageType] AS [PackageType],
-			FN.[Name] AS [PackageReference],
-			FN.Version AS [PackageVersion]
+SELECT		MAX(Rx.Timestamp) AS [Timestamp],
+			Rx.Organization AS [Organization],
+			Rx.[ProjectId] AS [ProjectId],
+			Rx.[Project] AS [ProjectName],
+			Rx.[RepositoryId] AS [RepositoryId],
+			Rx.[Repository] AS [RepositoryName],
+			Rx.[FileType] AS [FileType],
+			Rx.[FileId] AS [FileId],
+			Rx.[FilePath] AS [FilePath],
+			Rx.AzureFunction AS AzureFunctionRuntime,
+			Rx.IsAzureFunction AS IsAzureFunction,
+			Rx.FrameworkVersion AS [FrameworkVersion],
+			Rx.IsEol AS [IsEol],
+			Rx.EolDate AS [EolDate],
+			Rx.Eol3Months AS [Eol3Months],
+			Rx.Eol6Months AS [Eol6Months],
+			Rx.Eol9Months AS [Eol9Months],
+			Rx.Eol12Months AS [Eol12Months],
+			Rx.SupportedFramework AS [SupportedFramework],
+			Rx.TargetFramework AS [TargetFramework],
+			Rx.FileReference AS [FileReference],
+			Rx.UrlReference AS [UrlReference],
+			Rx.PackageType AS [PackageType],
+			Rx.PackageName AS [PackageReference],
+			Rx.PackageVersion AS [PackageVersion]
 
-FROM		Organizations O
-INNER JOIN	Projects P ON O.Id = P.OrganizationId
-INNER JOIN	Repositories R ON P.Id = R.ProjectId
-INNER JOIN	Files F ON R.Id = F.RepositoryId
-INNER JOIN	FileTypes FT ON F.FileTypeId = FT.Id
-LEFT JOIN	FilePropertiesPivot FPP ON F.Id = FPP.FileId
-LEFT JOIN	FileReferences FR ON F.Id = FR.FileId AND FR.FileReferenceTypeId = 1
-LEFT JOIN	FileReferences FU ON F.Id = FU.FileId AND FU.FileReferenceTypeId = 2
-LEFT JOIN	FileReferences FN ON F.Id = FN.FileId AND FN.FileReferenceTypeId = 3
-LEFT JOIN	dotNetEndOfLife EOL ON (FPP.TargetFramework = EOL.[Version] OR FPP.TargetFrameworkVersion = EOL.[Version] 
-	-- this is to help with combining 'angular/core [major version]' as we do not need minor version for EOL calculation
-	OR FN.Name + ' ' + LEFT(Fn.Version, abs(charindex('.', Fn.Version) - 1))  = EOL.Version)
-WHERE		P.NoScan = 0
-	AND		R.NoScan = 0
-	AND		P.Deleted = 0
-	AND		R.Deleted = 0
+FROM		Reports Rx
+
+GROUP BY	Rx.Organization,
+			Rx.[ProjectId],
+			Rx.[Project],
+			Rx.[RepositoryId],
+			Rx.[Repository],
+			Rx.[FileType],
+			Rx.[FileId],
+			Rx.[FilePath],
+			Rx.AzureFunction,
+			Rx.IsAzureFunction,
+			Rx.FrameworkVersion,
+			Rx.IsEol,
+			Rx.EolDate,
+			Rx.Eol3Months,
+			Rx.Eol6Months,
+			Rx.Eol9Months,
+			Rx.Eol12Months,
+			Rx.SupportedFramework,
+			Rx.TargetFramework,
+			Rx.FileReference,
+			Rx.UrlReference,
+			Rx.PackageType,
+			Rx.PackageName,
+			Rx.PackageVersion
