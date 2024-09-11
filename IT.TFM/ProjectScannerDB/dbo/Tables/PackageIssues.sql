a6c35f82a4df5e571c93a607ce2f3f45f6c60bf4
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-09-11 13:18:00 -0700

    Creating table to store Package results

---
+++ IT.TFM/ProjectScannerDB/dbo/Tables/PackageIssues.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[PackageIssues](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ScanType] [nvarchar](20) NOT NULL,
+	[FileId] [int] NOT NULL,
+	[Framework] [nvarchar](50) NOT NULL,
+	[IsTopLevel] [bit] NOT NULL,
+	[PackageName] [nvarchar](max) NOT NULL,
+	[RequestedVersion] [nvarchar](50) NULL,
+	[ResolvedVersion] [nvarchar](50) NULL,
+	[LatestVersion] [nvarchar](50) NULL,
+	[Severity] [nvarchar](50) NULL,
+	[AdvisoryUrl] [nvarchar](max) NULL, 
+    CONSTRAINT [FK_PackageIssues_ToFiles] FOREIGN KEY (FileId) REFERENCES [dbo].[Files](Id)
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[PackageIssues] ADD  CONSTRAINT [DF_PackageIssues_IsTopLevel]  DEFAULT ((0)) FOR [IsTopLevel]
+GO
