c0b3ef66f8754330f0537700ae283f9f5762c7ad
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-10-11 09:30:51 -0700

    Initial set of NuGet tables defined

---
+++ IT.TFM/ProjectScannerDB/dbo/Tables/NuGetFeeds.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[NuGetFeeds]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL,
+    [Url] NVARCHAR(MAX) NOT NULL, 
+    [FeedUrl] NVARCHAR(MAX) NOT NULL DEFAULT '', 
+    [ProjectId] INT NULL, 
+    CONSTRAINT [FK_NuGetFeeds_ToProjects] FOREIGN KEY ([ProjectId]) REFERENCES [Projects]([Id]) 
+)
---
+++ IT.TFM/ProjectScannerDB/dbo/Tables/NuGetPackages.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[NuGetPackages]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RepositoryId] INT NULL,
+    [NuGetFeedId] INT NOT NULL,
+    [Name] NVARCHAR(MAX) NOT NULL, 
+    [Version] NVARCHAR(50) NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL DEFAULT '', 
+    [Authors] NVARCHAR(MAX) NOT NULL DEFAULT '', 
+    [Published] DATETIME NOT NULL, 
+    [ProjectUrl] NVARCHAR(MAX) NULL, 
+    [Tags] NVARCHAR(MAX) NOT NULL DEFAULT '', 
+    CONSTRAINT [FK_NuGetPackages_ToRepositories] FOREIGN KEY ([RepositoryId]) REFERENCES [Repositories]([Id])
+)
