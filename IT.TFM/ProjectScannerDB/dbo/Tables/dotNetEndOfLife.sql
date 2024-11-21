74fbd3702ee57c7da5b13db35b2700a34b7f9209
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-11-21 12:38:25 -0800

    Commiting updated DB schema and data. Also doing some package updates.

--- IT.TFM/ProjectScannerDB/dbo/Tables/dotNetEndOfLife.sql
+++ IT.TFM/ProjectScannerDB/dbo/Tables/dotNetEndOfLife.sql
@@ -4,5 +4,8 @@
     [EOL] DATE NULL, 
     [ExtendedEOL] DATE NULL, 
     [Display] NVARCHAR(50) NOT NULL, 
-    [IsTargetVersion] BIT NOT NULL DEFAULT 0
+    [IsTargetVersion] BIT NOT NULL DEFAULT 0, 
+    [ReleaseDate] DATE NULL,
+    [FrameworkProductId] INT NULL, 
+    CONSTRAINT [FK_dotNetEndOfLife_ToFrameworkProduct] FOREIGN KEY ([FrameworkProductId]) REFERENCES [FrameworkProducts]([Id])
 )
