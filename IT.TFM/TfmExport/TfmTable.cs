6ccff54ccc2350a6b017a103fd4848f11e669bed
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2024-11-26 10:38:37 -0800

    Adding new tools folder, with TfmExport app. This app will allow me to extract the data from a TFM database, so that it can be applied to another for development work.

---
+++ IT.TFM/TfmExport/TfmTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfmExport
+{
+    public class TableColumn
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public bool UseQuotes { get; set; } = false;
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+
+    public class TfmTable
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public bool HasIdentity { get; set; } = false;
+
+        public List<TableColumn> Columns { get; set; } = [];
+
+        public override string ToString()
+        {
+            return $"{Name}: HasIdentity = {HasIdentity}, Columns = {Columns}";
+        }
+    }
+}
