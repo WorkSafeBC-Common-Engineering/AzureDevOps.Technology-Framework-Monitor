3083ea331900b9f9f1fb4cb83b130837db9f5efa
Andreas Mertens <115173538+AndreasNVI@users.noreply.github.com>
2024-11-27 08:29:14 -0800

    Commiting updated DB schema and data. (#36)
    Also doing some package updates.
    
    Co-authored-by: Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>

---
+++ IT.TFM/ProjectScannerDB/dbo/Tables/FrameworkProducts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[FrameworkProducts]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ProductName] NVARCHAR(50) NOT NULL, 
+    [Source] NVARCHAR(50) NULL, 
+    [SNOWReferenceId] NVARCHAR(50) NULL
+)
