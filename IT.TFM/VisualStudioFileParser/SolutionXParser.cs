6063ec92953bfabb7be1f9007ab4c4e1fd437bbb
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2026-02-25 11:23:40 -0800

    Adding parsing for the Configuration Also some cleanup to remove unused code, C# optimizations

--- IT.TFM/VisualStudioFileParser/SolutionXParser.cs
+++ IT.TFM/VisualStudioFileParser/SolutionXParser.cs
@@ -3,23 +3,21 @@
 using ProjectData;
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml;
 
 namespace VisualStudioFileParser
 {
     class SolutionXParser : Parser.ParseXmlFile, IFileParser
     {
-        #region Private Members
 
-        private const string propertyFileFormat = "FileFormat";
-        private const string propertyVSVersion = "VisualStudioVersion";
-        private const string propertyMinVSVersion = "MinimumVisualStudioVersion";
+        #region Private Members
 
         private const string xmlSolution = "Solution";
         private const string xmlProperties = "Properties";
         private const string xmlFolder = "Folder";
+        private const string xmlConfigurations = "Configurations";
+        private const string xmlBuildType = "BuildType";
+        private const string xmlPlatform = "Platform";
         private const string xmlAttrName = "Name";
         private const string xmlAttrPath = "Path";
         private const string xmlAttrType = "Type";
@@ -45,6 +43,7 @@ namespace VisualStudioFileParser
             }
 
             ParseSolutionProperties(rootNode, file);
+            ParseConfigurations(rootNode, file);
             ParseProjects(rootNode, file);
             ParseFolders(rootNode, file);
         }
@@ -55,8 +54,7 @@ namespace VisualStudioFileParser
 
         private static void ParseSolutionProperties(XmlElement rootNode, FileItem file)
         {
-            var propertiesNode = rootNode.SelectSingleNode(xmlProperties) as XmlElement;
-            if (propertiesNode == null)
+            if (rootNode.SelectSingleNode(xmlProperties) is not XmlElement propertiesNode)
             {
                 return;
             }
@@ -65,6 +63,32 @@ namespace VisualStudioFileParser
             if (nameNode != null && !string.IsNullOrWhiteSpace(nameNode.InnerText))
             {
                 file.AddProperty("SolutionName", nameNode.InnerText);
+            }
+        }
+
+        private static void ParseConfigurations(XmlElement rootNode, FileItem file)
+        {
+            if (rootNode.SelectSingleNode(xmlConfigurations) is not XmlElement configurationsNode)
+            {
+                return;
+            }
+
+            if (configurationsNode.SelectSingleNode(xmlBuildType) is XmlElement buildTypeNode)
+            {
+                var buildType = buildTypeNode.Attributes[xmlAttrName]?.Value;
+                if (!string.IsNullOrWhiteSpace(buildType))
+                {
+                    file.AddProperty("Configuration_BuildType", buildType);
+                }
+            }
+
+            if (configurationsNode.SelectSingleNode(xmlPlatform) is XmlElement platformNode)
+            {
+                var platform = platformNode.Attributes[xmlAttrName]?.Value;
+                if (!string.IsNullOrWhiteSpace(platform))
+                {
+                    file.AddProperty("Configuration_Platform", platform);
+                }
             }
         }
 
