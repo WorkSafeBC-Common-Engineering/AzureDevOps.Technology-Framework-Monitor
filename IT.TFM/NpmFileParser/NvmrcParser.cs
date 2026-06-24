56f674675f9848870fd4b8dd920a8a1e8e05524d
Andreas Mertens <Andreas Mertens Andreas.Mertens@worksafebc.com>
2026-06-24 12:29:16 -0700

    Added the NvmrcParser to parse the version an major version number from the .nvmrc file

---
+++ IT.TFM/NpmFileParser/NvmrcParser.cs
@@ -0,0 +1,47 @@
+﻿using Parser.Interfaces;
+
+using ProjectData;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NpmFileParser
+{
+    public class NvmrcParser : IFileParser
+    {
+        #region Private Members
+
+        private const string prefix = "v";
+        private const string versionKey = "Version";
+        private const string majorVersionKey = "MajorVersion";
+
+        #endregion
+
+        #region IFileParse Implementation
+
+        void IFileParser.Initialize(object data)
+        {
+            // no op
+        }
+
+        void IFileParser.Parse(FileItem file, string[] content)
+        {
+            var version = content[0].Trim();
+            file.AddProperty(versionKey, version);
+
+            if (version.StartsWith(prefix))
+            {
+                version = version[prefix.Length..];
+            }
+
+            var majorVersion = version.Split('.')[0];
+            if (int.TryParse(majorVersion, out _))
+            {
+                file.AddProperty(majorVersionKey, majorVersion);
+            }            
+        }
+
+        #endregion
+    }
+}
