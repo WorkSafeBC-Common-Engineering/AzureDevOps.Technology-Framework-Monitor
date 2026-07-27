8e5390a0e8bb8fa001dffc2b0c69830ddf4438a3
Vis Naidu <vn58615@worksafebc.com>
2026-07-27 14:21:58 -0700

    DB updates for EOL and FilePropertiesPivot, as per comment by AndreasNVI

--- IT.TFM/ProjectScannerDB/dbo/Views/FilePropertiesPivot.sql
+++ IT.TFM/ProjectScannerDB/dbo/Views/FilePropertiesPivot.sql
@@ -4,7 +4,12 @@ SELECT		Id AS FileId, AndroidApp, DBSchemaProvider, Error, ExeName32, FileFormat
 						  MajorVersion, MinimumVisualStudioVersion, MinorVersion, OutputType,
 						  ProjectVersion, RevisionVersion, SchemaVersion, Sdk, TargetFramework,
 						  TargetFrameworkVersion, NodeMajorVersion, TargetLanguage, ToolsVersion, [Type], VisualStudioVersion,
-						  [ApiKey Open Secret], [DB Open Secret], AzureFunction
+						  [ApiKey Open Secret], [DB Open Secret], AzureFunction,
+						  PythonVersionDockerfile, PythonMajorVersionDockerfile,
+						  PythonVersionProjectToml, PythonMajorVersionProjectToml, 
+						  PythonVersionSetupCfg, PythonMajorVersionSetupCfg,
+						  PythonVersionSetupPy, PythonMajorVersionSetupPy, 
+						  PythonVersion, PythonMajorVersion 
 FROM		(	SELECT		F.Id, FP.Name, ISNULL(FP.Value, '') AS Value
 				FROM		Files F
 				INNER JOIN	FileProperties FP ON F.Id = FP.FileId AND FP.PropertyTypeId = 1
@@ -16,5 +21,10 @@ PIVOT
 								MajorVersion, MinimumVisualStudioVersion, MinorVersion, OutputType,
 								ProjectVersion, RevisionVersion, SchemaVersion, Sdk, TargetFramework,
 								TargetFrameworkVersion, NodeMajorVersion, TargetLanguage, ToolsVersion, [Type], VisualStudioVersion,
-								[ApiKey Open Secret], [DB Open Secret], AzureFunction)
+								[ApiKey Open Secret], [DB Open Secret], AzureFunction, 
+								PythonVersionDockerfile, PythonMajorVersionDockerfile,
+								PythonVersionProjectToml, PythonMajorVersionProjectToml, 
+								PythonVersionSetupCfg, PythonMajorVersionSetupCfg,
+								PythonVersionSetupPy, PythonMajorVersionSetupPy, 
+								PythonVersion, PythonMajorVersion )
 			) AS PivotTable
